f830281723896bf00537c8addc6946902cb5b9bc
Shernobyl <abdullahelsharnouby@gmail.com>
2022-05-18 21:15:12 +0200

    Added api handlers

---
+++ src/main/java/Api/Server.java
@@ -0,0 +1,26 @@
+package Api;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Server {
+    public static void main(String[] args) {
+        int port = 9000;
+        HttpServer server = null;
+        try {
+            server = HttpServer.create(new InetSocketAddress(port), 0);
+            System.out.println("server started at " + port);
+            server.createContext("/", new RootHandler());
+            server.createContext("/echoHeader", new EchoHeaderHandler());
+            server.createContext("/echoGet", new EchoGetHandler());
+            server.createContext("/echoPost", new EchoPostHandler());
+            server.setExecutor(null);
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
